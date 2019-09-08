91d117c120312769ac1adb19f04d16e93f8205a8
Christine Frank <christine.braun13@gmail.com>
2019-09-08 12:05:33 -0700

    Add config to enable bootstrap tooltips, remove prior attempts at other tooltip packages. Config html to use bootstrap tooltips

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/tooltip';
 import './styles.css';
 import {Galaxy} from './galaxy.js';
 import solarSystemImg from './images/solarsystem';
@@ -9,4 +10,7 @@ $(document).ready(function(){
   let ssImg = $("#solarSystem");
   ssImg.attr("src", solarSystemImg);
   let galaxy = new Galaxy();
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
 });
