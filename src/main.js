114fe6c31a93d1562adac53a3d26309f23d89e28
Christine Frank <christine.braun13@gmail.com>
2019-09-08 18:40:09 -0700

    Add js to show error message when form has missing input. Show age input when form is complete. Add js to update slider values when moved

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/tooltip';
 import './styles.css';
-import {Galaxy} from './galaxy.js';
 import solarSystemImg from './images/solarsystem';
 import planetImg from './images/planet';
+import {Galaxy} from './galaxy.js';
+import {checkAgeInput, convertAge, checkFormInput, calcExpectedEarthLife, calcExpectedPlanetLife, calcAgeDiff} from './calculator.js';
 
 $(document).ready(function(){
   let ssImg = $("#solarSystem");
@@ -15,7 +16,50 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip()
   });
   updateSliderValues();
+
+  $("#info").submit(function(event){
+    event.preventDefault();
+    const quizResults = getQuizResults();
+    if(!checkFormInput(quizResults)){
+      missingInput();
+    } else {
+      makeAgeForm();
+    }
+    $('#results').show();
+  });
+
+  $(".slidecontainer").on("input", function(){
+    updateSliderValues();
+  });
 });
+
+function makeAgeForm(){
+  const ageForm = `
+    <form id="age">
+      <label for="age">Enter your age:</label>
+      <input id="age" type="text">
+      <button type="submit" name="button"> Submit Age</button>
+    </form>
+  `
+  $('#results').html(ageForm);
+}
+
+function missingInput(){
+  $("#results").text("Please select options from all sections of quiz.");
+  $("#results").addClass('missing-input');
+}
+
+function getQuizResults(){
+  const exercise = $("input:radio[name=exercise]:checked").val();
+  const meal = $("input:radio[name=meal]:checked").val();
+  const other = $("input:checkbox[name=influence]:checked").toArray();
+  const technical = $("#technical").val();
+  const social = $("#social").val();
+  const adapt = $("#adapt").val();
+  const gov = $("input:radio[name=government]:checked");
+  const inputArray = [exercise, meal, other, technical, social, adapt, gov];
+  return inputArray;
+}
 
 function updateSliderValues(){
   const technical = $("#technical").val();
