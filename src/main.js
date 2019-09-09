d9ffef9fe8ddeaa343220826b2ee19564be75c6a
Christine Frank <christine.braun13@gmail.com>
2019-09-08 17:53:00 -0700

    Add planet as slider thumb. Update style of sliders

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/js/dist/tooltip';
 import './styles.css';
 import {Galaxy} from './galaxy.js';
 import solarSystemImg from './images/solarsystem';
+import planetImg from './images/planet';
 
 $(document).ready(function(){
   let ssImg = $("#solarSystem");
