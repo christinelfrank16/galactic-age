8698376aee6e13174249f1ca5987e5bea48b3284
Christine Frank <christine.braun13@gmail.com>
2019-09-11 20:52:38 -0700

    Add content to planet results for mars to uranus

--- src/main.js
+++ src/main.js
@@ -109,10 +109,45 @@ function makePlanetResults(age, planetName, planetAge, expectedLifeSpan, ageDiff
     case "earth":
       resultsString += ' Previous generations used up the resources on what used to be called "Blue Planet". Now everyone calls it the Brown Planet, and many don\'t know it as Earth anymore. You are stuck here because your parent\'s parents were not wealthy enough to get on the rockets while they were affordable.';
       if(ageDiff > 0){
-        resultsString += ` You are no longer young and spry, about <strong>${ageDiff}</strong> past your expected life. Your body aches and your bones hurt. You are living on borrowed air - literally - (it's sourced from the other planets) and you've heard it may not be imported for much longer...`
+        resultsString += ` You are no longer young and spry, about <strong>${ageDiff}</strong> past your expected life. Your body aches and your bones hurt. You are living on borrowed air - literally - (it's sourced from the other planets) and you've heard it may not be imported for much longer...`;
       } else {
-        resultsString += ` You have worked hard mining what is left of the Earth's core for precious metals, and have made barely enough to purchase tickets for you and your family to go to Venus. You expect to live to <strong>${expectedLifeSpan}</strong>, but that will be paused while in cryo-sleep on the way there.`
+        resultsString += ` You have worked hard mining what is left of the Earth's core for precious metals, and have made barely enough to purchase tickets for you and your family to go to Venus. You expect to live to <strong>${expectedLifeSpan}</strong>, but that will be paused while in cryo-sleep on the way there.`;
       }
+      break;
+    case "mars":
+      resultsString += ` This is where the past people of Earth first tried to habitat after realizing they were pulling their own home apart. It is come to be known as the 'center of the universe', mostly because the government Center is here.`;
+      if(ageDiff > 0){
+        resultsString += ` Your family was one of the first to populate the planet, so you have had a generous upbringing. That explains why you have lived <strong>${ageDiff}</strong> past your expected life, and still going. With your cushy life, you're not complaining.`;
+      } else {
+        resultsString += ` Being the highest planet density has its drawbacks, and you see them every day: you are a service worker tending to the poor and downtrodden. Although you are not too far above them yourself. You expect to live to <strong>${expectedLifeSpan}</strong>, and that you'll have to work almost all of that time. The people you see and help everyday are a reminder that it could be worse, and also reminders for you to keep working...`;
+      }
+      break;
+    case "jupiter":
+      resultsString += ` This planet was more 'experimental' when first attempting to populate it. The gasseous atomosphere and widely variable temperature swings made it difficult to live here. The scientists did come up with a liveable solution: floating pods. The pods maintain the correct bouyancy to stay in the region of atomosphere that is liveable, and have been somehow tethered to the planet's core.`;
+      if(ageDiff > 0){
+        resultsString += ` You can't argue the scientists made life here liveable; you've lived <strong>${ageDiff}</strong> past your expected life span. Not to say expecting to live to <strong>${expectedLifeSpan}</strong> is a special thing, you probably would have expected to live longer on a different planet.`;
+      } else {
+        resultsString += ` There are days you fear you won't live to <strong>${expectedLifeSpan}</strong>. The Storms that whirl around Jupiter can be deadly in the bouyant pods if you are not expecting them. There are stories of pods loosing their anchors and the people inside dying from heat exposure, or freezing, depending on which region of atomosphere where the pod was tossed. They didn't get to their escape ship in time. Ever since hearing that story and the pod jostles, you map your escape route ...`;
+      }
+      break;
+    case "saturn":
+      resultsString += ` Many folk have come here for the beauty of the Rings. Depending on the angle of light from the sun, they can be spectacular and inspiring.`;
+      if(ageDiff > 0){
+        resultsString += ` This is why you used up all of your money to come here from Mars, and it was well worth it. Most days, you get at least one clear glimpse of them and they put you in awe every time. Maybe that is why you have lived </strong>${ageDiff}</strong> past your expected life span: those few moments of beauty.`;
+      } else {
+        resultsString += ` But they don't hold those or similar emotions for you. Your ex decided that being able to see them almost daily would be a life-changer. Turns out it was true: your ex took all your money and moved back to Mercury because the pods bobbled too much. ... Didn't say a word to you either. You expect to live to <strong>${expectedLifeSpan}</strong>, but it won't be happily and will probably be alone.`;
+      }
+      break;
+    case "uranus":
+      resultsString += ` Whispers of unrest are common here. They blend with the whirring of machinery that processes the natural atomosphere. It is common to see weaponry out in the open as deterrents`;
+      if(ageDiff > 0){
+        resultsString += ` - a method you know works well. You openly carry a laser gun, and have so far lived <strong>${ageDiff}</strong> past your expected life span. It's a rough world, so in your mind, you have to take a rough stance against it; the universe isn't going to be soft and cuddly and nice to you, may as well be prepared.`;
+      } else {
+        resultsString += `. You just try to keep your head down around those folks. You've only lived so long, and expect to live to <strong>${expectedLifeSpan}</strong>. Just because rebels talk of uprising against the government, and how unfair it is, doesn't mean you have to act on it. Even if you agree maybe more than a little...`;
+      }
+      break;
+    case "neptune":
+      break;
   }
 
   return resultsString;
